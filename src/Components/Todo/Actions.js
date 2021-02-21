11e1280426d182794834fb3e24d03d7144aec5b6
sofiree <muosophia0@gmail.com>
2021-02-21 10:49:29 +0100

    FEAT: created actions for components

---
+++ src/Components/Todo/Actions.js
@@ -0,0 +1,19 @@
+// create a type of action 
+// this action allows the user to create a todo
+export const CREATE_TODO ='CREATE_TODO';
+
+// create a defined function to call the action and what the action will carry in the payload as a object
+export const createTodo =text=>({
+    type: CREATE_TODO,
+    payload: {text},
+});
+
+// create an action type that allows the user delete/remove a todo
+
+export const DELETE_TODO = 'DELETE_TODO';
+export const deleteTodo = text =>({
+    type: DELETE_TODO,
+    payload: {text},
+});
+
+// reducers keep track of the state of the actions
